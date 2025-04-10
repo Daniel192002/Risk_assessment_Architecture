40e1c8de4f4b4bf6e744135b67805105d867762b
daniel <dani19fgz@gmail.com>
2025-04-10 15:50:50 +0200

    Prueba de la fase 1

--- tests/script_ping.py
+++ tests/script_ping.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     
     time.sleep(5)
     
-    command = f"ping -c 4 192.168.0.4"
+    command = f"ping -c 4 192.168.1.4"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output, _ = process.communicate()
     print("Segundo comando:")
