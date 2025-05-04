b4071cdf6838b933764274e1be5d86e9dd26467f
daniel <dani19fgz@gmail.com>
2025-05-05 00:34:08 +0200

    Prueba de gvmd

--- tests/script_ping.py
+++ tests/script_ping.py
@@ -6,7 +6,7 @@ INTERVALO = 60
 if __name__ == "__main__":
   while True:
     
-    command = f"ping -6 -c 4 ff02::1%eth1"
+    command = f"ping -6 -c 4 ff02::1%enp0s8"
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     output, _ = process.communicate()
     print("Primer Comando:")
