06c1239ddde82c6e0e4cfdb12a8ca849b84598ff
daniel <dani19fgz@gmail.com>
2025-05-17 13:51:57 +0200

    Prueba de cves

--- src/databaseManager.py
+++ src/databaseManager.py
@@ -30,6 +30,22 @@ class DatabaseManager:
         query = "SELECT mac, ipv4, ipv6 FROM assets"
         self.cursor.execute(query)
         return self.cursor.fetchall()
+    
+    def insert_vulnerability(self, ipv4, cve):
+        try:
+            cursor = self.conn.cursor()
+            query = "INSERT INTO vulnerabilities (ipv4, cve) VALUES (%s, %s)"
+            cursor.execute(query, (ipv4, cve))
+            self.conn.commit()
+        except mariadb.Error as e:
+            print(f"Error insertando vulnerabilidad: {e}")
+    
+    
+    def cve_exists(self, ipv4, cve):
+        cursor = self.conn.cursor()
+        query = "SELECT 1 FROM vulnerabilities WHERE ipv4 = %s AND cve = %s LIMIT 1"
+        cursor.execute(query, (ipv4, cve))
+        return cursor.fetchone() is not None
 
     def close(self):
         if self.conn:
