744f620d33b450987040e3b23a701de3df994477
daniel <dani19fgz@gmail.com>
2025-05-20 13:13:21 +0200

    Prueba de cves

--- src/main.py
+++ src/main.py
@@ -2,13 +2,7 @@
 from riskController import RiskController
 import subprocess
 
-def set_permissions():
-    try:
-        subprocess.run(["chmod", "777", "/run/gvmd/gvmd.sock"], check=True)
-        print("[+] Permisos modificados correctamente")
-    except subprocess.CalledProcessError as e:
-        print(f"[!] Error al modificar permisos: {e}")
+
 if __name__ == "__main__":
-    set_permissions()
     controller = RiskController()
     controller.execute_risk_analysis()
