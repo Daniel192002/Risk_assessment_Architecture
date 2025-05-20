a78c06f8071a59421eca68b6abc3ef376b63bea9
daniel <dani19fgz@gmail.com>
2025-05-20 14:09:04 +0200

    Prueba de cves

--- src/riskController.py
+++ src/riskController.py
@@ -29,11 +29,11 @@ class RiskController:
                 print(f"[-] Dispositivo ya existente (ignorando): {mac} - IPv4: {ipv4} - IPv6: {ipv6}")
         
         #ESCANEO DE VULNERABILIDADES
-        existing_devices = self.db.get_devices()
         for mac, addrs in existing_devices:
-            ipv4 = addrs["IPv4"]
-            if ipv4:
-                cves = self.vulnerability_scanner.scan(ipv4)
+            mac = existing_devices[1]
+            ip = existing_devices[2]
+            if ip:
+                cves = self.vulnerability_scanner.scan(ip)
                 print(f"[+] CVES: {cves}")
                 for cve, severity in cves:
                     if not self.db.cve_exists(ipv4, cve):
