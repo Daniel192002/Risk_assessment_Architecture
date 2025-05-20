4319489eae3e11542401ec645b7b68c3304b14ab
daniel <dani19fgz@gmail.com>
2025-05-20 11:48:07 +0200

    Prueba de cves

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
-from asset_controller import AssetController
-from vulnerabilityScanner import VulnerabilityScanner
 
-INTERFAZ = "eth1"
+from riskController import RiskController
+
 
 if __name__ == "__main__":
-    dispositivos = AssetController.scan_network(INTERFAZ)
-    print("\n[DEBUG] Salida de IPv6:")
-    print(dispositivos)
-    if dispositivos:
-        print("\nDispositivos detectados:")
-        for mac, datos in dispositivos.items():
-            print(f"MAC: {mac}, IPv4: {datos['IPv4']}, IPv6: {datos['IPv6']}")
-    else:
-        print("\nNo se capturaron dispositivos en la red.")
 
-    scanner = VulnerabilityScanner()
-    scanner.scan_devices_from_db()
+    controller = RiskController()
+    controller.execute_risk_analysis()
