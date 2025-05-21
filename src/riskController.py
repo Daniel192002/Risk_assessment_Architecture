3a0adc7124f5b73c44c232b7b0a358417fb39d86
daniel <dani19fgz@gmail.com>
2025-05-21 23:06:00 +0200

    Prueba de cves

--- src/riskController.py
+++ src/riskController.py
@@ -18,29 +18,29 @@ class RiskController:
         devices = self.asset_controller.scan_network(INTERFAZ)
         existing_devices = self.db.get_devices()
         
-        # INSERTAR DATOS EN LA BASE DE DATOS
-        for mac, addrs in devices.items():
-            ipv4 = addrs["IPv4"]
-            ipv6 = addrs["IPv6"]
-            if (mac, ipv4, ipv6) not in existing_devices:
-                self.db.insert_device(mac, ipv4, ipv6)
-                print(f"[+] Dispositivo nuevo detectado: {mac} - IPv4: {ipv4} - IPv6: {ipv6}")
-            else:
-                print(f"[-] Dispositivo ya existente (ignorando): {mac} - IPv4: {ipv4} - IPv6: {ipv6}")
+        # # INSERTAR DATOS EN LA BASE DE DATOS
+        # for mac, addrs in devices.items():
+        #     ipv4 = addrs["IPv4"]
+        #     ipv6 = addrs["IPv6"]
+        #     if (mac, ipv4, ipv6) not in existing_devices:
+        #         self.db.insert_device(mac, ipv4, ipv6)
+        #         print(f"[+] Dispositivo nuevo detectado: {mac} - IPv4: {ipv4} - IPv6: {ipv6}")
+        #     else:
+        #         print(f"[-] Dispositivo ya existente (ignorando): {mac} - IPv4: {ipv4} - IPv6: {ipv6}")
         
-        #ESCANEO DE VULNERABILIDADES
-        for device in existing_devices:
-            mac = device[0]
-            ip = device[1]
-            if ip:
-                cves = self.vulnerability_scanner.scan(ip)
-                print(f"[+] CVES: {cves}")
-                for cve, severity in cves:
-                    if not self.db.cve_exists(ip, cve):
-                        self.db.insert_vulnerability(mac, ip, cve, severity)
-                        print(f"[+] Vulnerabilidad detectada: {cve} en {ip}")
-                    else:
-                        print(f"[-] Vulnerabilidad ya registrada (ignorando): {cve} en {ip}")
+        # #ESCANEO DE VULNERABILIDADES
+        # for device in existing_devices:
+        #     mac = device[0]
+        #     ip = device[1]
+        #     if ip:
+        #         cves = self.vulnerability_scanner.scan(ip)
+        #         print(f"[+] CVES: {cves}")
+        #         for cve, severity in cves:
+        #             if not self.db.cve_exists(ip, cve):
+        #                 self.db.insert_vulnerability(mac, ip, cve, severity)
+        #                 print(f"[+] Vulnerabilidad detectada: {cve} en {ip}")
+        #             else:
+        #                 print(f"[-] Vulnerabilidad ya registrada (ignorando): {cve} en {ip}")
         
         
         #Buscar y clasificar vulnerabilidades
