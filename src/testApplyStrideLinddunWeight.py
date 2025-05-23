b04f1921b5348780e601f16fc3c77957fa6552d8
daniel <dani19fgz@gmail.com>
2025-05-23 20:36:55 +0200

    Probando nvd

--- src/testApplyStrideLinddunWeight.py
+++ src/testApplyStrideLinddunWeight.py
@@ -21,11 +21,11 @@ class TestApplyStrideLinddunWeights(unittest.TestCase):
         linddun = ["Linkability", "Disclosure of Information"]
 
         expected_result = {
-            "Damage": 10.0,  # 5.0 + 2.0 + 2.2 = 9.2 → round 9.2
+            "Damage": 9.0,  # 5.0 + 1.5 + 1 + 1. 5= 9.2 → round 9.2
             "Reproducibility": 4.0,
-            "Exploitability": 7.8,  # 6.0 + 1.8
-            "AffectedUsers": 3.0,
-            "Discoverability": 4.5  # 2.5 + 2.0
+            "Exploitability": 7.5,  # 6.0 + 1.5 = 7.5
+            "AffectedUsers": 6.0, # 3.0 + 1 + 1 + 1 = 6.0
+            "Discoverability": 3.0  # 2.5 + 0.5 = 3.0
         }
 
         result = etd.apply_stride_linddun_weights(dread.copy(), stride, linddun)
